c47c55c939aba0029ac34295ffcc292476faef3b
Mehmet Sindi <mehmetsindi@gmail.com>
2025-11-29 15:52:33 +0300

    back

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -1,27 +1,36 @@
 <?php
 
-namespace Database\Factories;
+namespace Database\Seeders;
 
 use App\Models\User;
-use Illuminate\Database\Eloquent\Factories\Factory;
-use Illuminate\Support\Str;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
 
-/**
- * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\User>
- */
-class UserFactory extends Factory
+class DatabaseSeeder extends Seeder
 {
-    protected $model = User::class;
+    use WithoutModelEvents;
 
-    public function definition(): array
+    /**
+     * Seed the application's database.
+     */
+    public function run(): void
     {
-        return [
-            'name' => $this->faker->name(),                    // fake() → $this->faker
-            'email' => $this->faker->unique()->safeEmail(),    // fake() → $this->faker
-            'email_verified_at' => now(),
-            'password' => bcrypt('password'), // ya da Hash::make('password')
-            'remember_token' => Str::random(10),
-            'is_super_admin' => false,
-        ];
+        // User::factory(10)->create();
+
+        // Create Super Admin
+        User::factory()->create([
+            'name' => 'Mehmet Sindi',
+            'email' => 'mehmet@example.com', // Assuming email, user can change
+            'password' => bcrypt('password'),
+            'is_super_admin' => true,
+        ]);
+
+        // Create Test User
+        User::factory()->create([
+            'name' => 'Test User',
+            'email' => 'test@example.com',
+        ]);
+
+        $this->call(TaskSeeder::class);
     }
 }
